1a05bb7d7d913cc9e298a4058d91acea64ad834f
kahverengi <ahmet.duruer@yandex.com>
2020-10-04 14:24:13 +0200

    Init

---
+++ src/main/kotlin/com/ahmetduruer/systemengineeringwork1/Main.kt
@@ -0,0 +1,11 @@
+package com.ahmetduruer.systemengineeringwork1
+
+class Main {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello, Kotlin world!")
+        }
+    }
+}
